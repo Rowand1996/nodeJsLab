9d5936ad4f43a3f5c2db55c0f7d7b3d60b8f56b1
Rowan Douglas <rowanandvera@gmail.com>
2020-06-24 10:48:07 -0500

    completed lab correctly with promises

--- reddit.js
+++ reddit.js
@@ -1,33 +1,26 @@
-const path = require('path');
-const fs = require('fs');
-const rp = require('request-promise');
-
-let posts = [];
+const path = require('path'),
+        fs = require('fs'),
+        rp = require('request-promise');
 
 let dataPath = path.join(__dirname, './popular-articles.json');
 
-rp('https://reddit.com/r/popular.json', (err, res, body) => {
-    if (err) {
-        console.log(err);
-    }
-    // console.log(JSON.parse(body).data.children);
-
-    let children = JSON.parse(body).data.children;
-    let post = {}
-
-    for (let i = 0; i < children.length; i++) {
-        post[i] = {
-            postId: i,
-            postTitle: children[i].data.title,
-            postUrl: children[i].data.url,
-            postAuthor: children[i].data.author
-        }
-        posts.push(post[i]);
-    }
-    fs.writeFile(dataPath, JSON.stringify(posts), (err) => {
-        if (err) {
-            console.log(err);
-        }
-    });
-});
-
+rp('https://reddit.com/r/popular.json')
+    .then(res => JSON.parse(res).data.children)
+    .then(articles => {
+        let posts = [];
+        let post = {};
+        for (let i = 0; i < articles.length; i++) {
+            post[i] = {
+                postId: i,
+                postTitle: articles[i].data.title,
+                postUrl: articles[i].data.url,
+                postAuthor: articles[i].data.author
+            }
+            posts.push(post[i]);
+        };
+        fs.writeFile(dataPath, JSON.stringify(posts), (err) => {
+            if (err) throw err;
+            console.log("saved the file!");
+        });
+    })
+    .catch(err => console.log(err));
